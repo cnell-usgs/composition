923849985e947b2307eba69aca7d3ac59c788279
collnell <coll.s.nell#gmail.com>
2016-10-06 02:44:21 -0700

    heat maps need key

--- server.R
+++ server.R
@@ -1,7 +1,6 @@
-
+library(DT)
 library(devtools)
 library(shiny)
-library(DT)
 library(ggplot2)
 library(dplyr)
 library(d3heatmap)
@@ -11,30 +10,56 @@ library(reshape2)
 
 source_url("https://raw.githubusercontent.com/collnell/R_vis/master/theme_mooney.R")#ggplot theme
 doon<-as.data.frame(dune)
+doon.df<-round(doon,2)
 dune.dist<-vegdist(doon)
 dune.df<-round(as.matrix(dune.dist),2)
 
 shinyServer(function(input,output){
 
+  values<-reactiveValues(sxsp=dune.df)
+
+  observeEvent(input$runit,{
+    if(is.null(input$getcsv)) {
+      values$sxsp<-as.data.frame(dune)
+    } else{
+      inFile<-input$getcsv
+      temp.df<-read.csv(inFile$datapath)##needs to sites x species matrix, with 1st col as grouping var
+      temp.df[is.na(temp.df)]<-0
+      values$sxsp<-as.data.frame(temp.df)
+    }
+    
+    if (input$distin == "Bray-Curtis"){
+      distmat<-vegdist(values$sxsp,method="bray")
+      values$dist.df<-round(as.matrix(distmat))
+    }else{
+      distmat<-vegdist(values$sxsp,method="jaccard")
+      values$dist.df<-round(as.matrix(distmat))
+    }
+    
+  })
   
-  
-  values<-reactiveValues()
   
   output$heat<-renderD3heatmap({
 
-    he.ma<-d3heatmap(dune.dist,scale="column",colors="Spectral")
+    he.ma<-d3heatmap(values$dist.df,scale="column",colors="RdYlBu",breaks=5,Rowv=FALSE,Colv=FALSE,dendrogram='none')
     he.ma
   })
   
+  output$comm.viz<-renderD3heatmap({
+    comm.heat<-d3heatmap(doon,scale="column",colors="Spectral",breaks=5,Rowv=FALSE,Colv=FALSE,dendrogram='none')
+  })
+  
   output$dist.matrix<-renderDataTable(
-    dune.df,options=list(dom='t')%>%
-      formatRound(columns=names(dune.df),digits=2)
+    values$dist.df,extensions='FixedColumns',
+    options=list(pageLength=nrow(dune.df),dom='t',scrollX=TRUE,scrollY='400px',fixedColumns=list(leftColumns=1)),
+    class="compact"
     
   )
   
   output$comm.matrix<-renderDataTable(
-    doon,options=list(dom='t')%>%
-      formatRound(columns=names(doon),digits=2)
+    doon.df,extensions='FixedColumns',
+    options=list(pageLength=nrow(doon.df),dom='t',scrollX=TRUE,scrollY='400px',fixedColumns=list(leftColumns=1)),
+    class="compact"
     
   )
 
--- ui.R
+++ ui.R
@@ -1,9 +1,11 @@
 library(shinydashboard)
+library(d3heatmap)
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem("Community Matrix",tabName="topic",icon=icon("calculator",lib="font-awesome")),
     menuItem("Distance Matrix",tabName="dist",icon=icon("twitter", lib="font-awesome")),
+    menuItem("Data Wrangling",tabName="howto"),
     menuItem("Learn More",tabName="lit",icon=icon("book", lib="glyphicon")),
     br()
     
@@ -14,43 +16,48 @@ body <- dashboardBody(
   tabItems(
     tabItem(tabName="topic",
             fluidRow(
-              column(width=6,
+              column(width=4,
                      box(title=tags$b("What is a site x species community matrix?"), width=NULL,collapsible = TRUE,status="primary",
                          p("Sites are ROWS"),
-                         p("Species and COLUMNS"),
+                         p("Species abundances or absence/presence data in the COLUMNS"),
                          p("These can be transposed in some situation, but in general there is a column for each species and rows are sites/plots or the observations 
                            taken of the species. This format is commonly used in many types of analyses and does not pertain exclusively to species abundance data.
                            This also has applications to chemical analyses, genetic analyses, and really anything you can measure many variables for.
                            It is necessary to replicate species measurements to exhaustively survey them, in addition to feeling confident in your data.")),
-                     box(title = tags$b("Measuring Diversity"),width=NULL,status="primary",
-                         p("this is what a matrix looks like"),
-                         p("Upload a site x species matrix for a community. Pressing 'Run' will visualize the data in a heatmap"),
+                     box(title = tags$b("Abundance Matrix"),width=NULL,status="primary",
+                         p("Community matrices are commonly used to display species abundances across multiple sites or observations. Shown to the right is a heatmap
+                           of the abundance values for the species measured in the 'dune' dataset in R. Each row of the data are XXX and each column of the data matrix are 
+                           species. The colors in the heatmap vary to reflect the species abundances."),
+                         p("A heatmap could also be used to display many variables at one time."),
+                         p("In the heatmap the columns and rows are the same, but hte values are visualized with a color scale that is not pictured. "),
+                         p("Upload your won site x species matrix of communitymeasurements. Pressing 'Run' will re-run the heatmap visualization for the uploaded data and 
+                           print the corresponding data in the table."),
                          fileInput("getcsv","Upload data",multiple=FALSE,accept=c("text/csv","text/comma-separated-values,text/plain",".csv"))
-                     ),
+                     )
+              ),
+              column(width=8,
+                     box(title = tags$b("Visualizing the data matrix"),width=NULL,status="primary",
+                         d3heatmapOutput("comm.viz")),
                      box(title=tags$b("Data Matrix"), width=NULL,
                          dataTableOutput("comm.matrix"))
-              ),
-              column(width=6,
-                     box(title = "Heatmap",width=NULL,status="primary",
-                         d3heatmapOutput(" ")),
-                     box(title="??",width=NULL,
-                         p("mouseover data?")
-                         ))
               )
             
-    ),
+    )),
     tabItem(tabName="lit",
             fluidRow(
               column(width=6,
-                     box(title=tags$b("Literature"),width=NULL,
+                     box(title=tags$b("Learn More"),width=NULL,
                          p("Magurran, A. E. 1988. Ecological Diversity and its Measurement. Princeton University Press, Princeton, NJ."),
                          p(""),
                          a("See pdf", href="web address",target="_blank"))),
               column(width=6,
                      box(title=tags$b("R packages"),width=NULL,
-                         p("Several R packages have been developed for XXXXX.
-                           The following were used in the development of this app."),
-                         h4("package"),
+                         p("The following R packages draw heatmaps and distance matrices:"),
+                         h4("D3heatmap"),
+                         h4("heatmaply"),
+                         h4("phyloseq"),
+                         h4("vegan"),
+                         
                          p("description."),
                          a("Documentation",href="docadd",target="_blank")),
                      box(title=tags$b("This app"), solidHeader=TRUE,status="warning",width=NULL,
@@ -67,31 +74,37 @@ body <- dashboardBody(
     
     tabItem(tabName="dist",
             fluidRow(
-              column(width=5,
+              column(width=4,
                      box(title=tags$b("Distance Matrix"),width=NULL,
-                     p("description"),br()
+                     p("Using you community matrix you can create a distance matrix, which calculates pairwise similarity/dissimilarity among your sites or sampling units.
+                       This tells your overall information about how they are similar, no shit. The diagonal line on the distance matrix falls where sites are compared to themselves.
+                       Because of this they are either 1 or zero, depending on how your matrix was calculated"),br()
                      ),
-                     box(title="Methods",width=NULL,
+                     box(title=tags$b("Methods"),width=NULL,
                          p("Species composition of an ecological community tells us more about the community, beyond species richness and diversity. 
                            Many ecological communities contain a great diversity of species, many of which may be rare or uncommon. The next layer of information 
                            is in species composition. One way to visualize large amount of data on many species of variables is by creating a distance matrix.
                            A distance matrix is a way of comparing relative species composition and similarity/dissimilarity among sites/plots."),
-                         p("Bray-curtis and Jaccard are the main dissimilarity indices. Bray-curtis incorporates species abundance while Jaccard does not
-                           and is absence/presence data. Both provide valuable information. "),
+                         p("Bray-curtis and Jaccard are the main dissimilarity indices. These are rank-order similar meaning XXX. Bray-curtis incorporates species abundance while Jaccard does not
+                           and is absence/presence data. Both provide valuable information. In the distance matrix the species columns are gone and columns and rows
+                           both correspond to the sites, or rows of the data file. Each value indicates the pairwise degree of similarity for every site. These
+                           values mean this xxx"),
                          p("Bray-curtis"),
                          p("Jaccard"),
                          selectInput("distin","Dissimilarity Index",choices=c("Bray-Curtis","Jaccard")),
                          p("Data transformations may be necessary to normalize data and alleviate the effects of dominant species on your results. An alternative
                            approach is to convert species abundances to proportion data. Sqrt works. "),
-                         selectInput("df.tr","Data transformation:",choices=c("None","square root","log","proportions"),selected="none"),br(),
+                         selectInput("df.tr","Data transformation:",choices=c("None","square root","log","proportions"),selected="none"),
+                         actionButton("runit","Get distance matrix"),br(),
                          p("Upload a community matrix. Pressing 'Run' will calculate a distance matrix for your community and visualize it in a heatmap."),
-                         fileInput("getcsv","Upload data",multiple=FALSE,accept=c("text/csv","text/comma-separated-values,text/plain",".csv")))
+                         fileInput("getcsv","Upload data",multiple=FALSE,accept=c("text/csv","text/comma-separated-values,text/plain",".csv"))
+                     )
                      
             ),
-            column(width=7,
-              box(title = "Visualizing something",width=NULL,status="primary",
+            column(width=8,
+              box(title = tags$b("Visualizing the Distance Matrix"),width=NULL,status="primary",
                   d3heatmapOutput("heat")),
-              box(title="Distance Matrix",width=NULL,
+              box(title=tags$b("Distance Matrix"),width=NULL,
                   dataTableOutput("dist.matrix"))
                     
                      
@@ -99,8 +112,19 @@ body <- dashboardBody(
     )
     
     
-  )
-))
+  ),
+  tabItem(tabName="howto",
+          fluidRow(
+            box(title="Prepping your data", width = NULL,
+                p("In the field it is common to take data in the long format. It is not necessarily convenient or paper friendly to print out a species matrix for 
+                  a large number of speices that may all be fairly rare, thus the page stay unused. "),
+                p("The following steps will transform your data from the long format to short format (community matrix) in R. Long format means that each line of your data 
+                  are an observation/value of a single species."),
+                p(tags$code("library(reshape2)")),
+                p(tags$code("long.data<-read.csv('filename.csv')")),
+                p(tags$code(""))
+          ))
+)))
 # Put them together into a dashboardPage
 dashboardPage(skin = "black",
               dashboardHeader(title = "Measuring Diversity", titleWidth=250),
